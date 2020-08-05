564c2e91ba3b05ca429f2a964d9cfe79d341ba22
abraham <abraham@MacBook-Air-de-abraham.local>
2020-08-05 07:12:00 -0500

    Fix linter

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+// HelloServer expose endpoint to greet
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
 }
